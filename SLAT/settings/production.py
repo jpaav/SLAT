2141d1806b9895c9244894ac20d179b865b4095b
jpaav <joepaavola@gmail.com>
2018-09-25 09:42:23 -0400

    Fetches secret key and debug from heroku if on production

--- SLAT/settings/production.py
+++ SLAT/settings/production.py
@@ -3,3 +3,10 @@ import dj_database_url
 from .base import *
 
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+
+SECRET_KEY = os.environ['SECRET_KEY']
+
+if os.environ['DEBUG'].lower() in ['true', 't', 'y', 'yes']:
+	DEBUG = True
+else:
+	DEBUG = False
