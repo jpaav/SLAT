100b3ff93cad5dd70210f380ef526f1355273f3d
jpaav <joepaavola@gmail.com>
2018-09-25 10:23:11 -0400

    Registered book and transaction with django admin

--- server/admin.py
+++ server/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 from .models import *
 
-
-admin.register(Book)
-admin.register(Transaction)
-admin.register(Token)
+admin.site.register(Book)
+admin.site.register(Transaction)
